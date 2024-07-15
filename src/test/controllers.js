08a635b899ba642adfdbd9b9f52f96c5f6b18eab
Rowan12628 <170511926+Rowan12628@users.noreply.github.com>
2024-07-15 11:50:48 +0100

    feature: add isData middleware

--- src/test/controllers.js
+++ src/test/controllers.js
@@ -1,6 +1,7 @@
 const postTest = async (req, res) => {
+  console.log("postTest controller hit and username: ", req.body.username);
   try {
-    await res.status(201).json({ message: "success", body: req.body });
+    res.status(201).json({ message: "success", body: req.body });
   } catch (error) {
     res.status(500).json({ message: error.message, error: error });
   }
