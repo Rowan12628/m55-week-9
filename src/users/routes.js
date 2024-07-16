71258b158674336d00f5fa00ac525a39d77d9433
Rowan12628 <170511926+Rowan12628@users.noreply.github.com>
2024-07-16 11:39:11 +0100

    feature: add hashPass auth.js

--- src/users/routes.js
+++ src/users/routes.js
@@ -3,11 +3,13 @@ const userRouter = Router();
 
 const { signUp, login, getAllUsers } = require("./controllers");
 
+const { hashPass } = require("../middleware/auth");
+
 userRouter.get("/test", async (req, res) => {
   res.status(200).json({ message: "test okey dokey!" });
 });
 
-userRouter.post("/signUp", signUp);
+userRouter.post("/signUp", hashPass, signUp);
 
 userRouter.post("/login", login);
 
